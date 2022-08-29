08501b32f59ef962852ff31e200ebc9871922d9a
Gajo Petrovic <gajopetrovic@gmail.com>
2022-08-29 20:20:50 +0900

    add win screen

--- src/text_display.rs
+++ src/text_display.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
 
+use crate::level::LevelInfo;
+
 pub struct TextDisplayPlugin;
 
 impl Plugin for TextDisplayPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup_text);
+        app.add_startup_system(setup_text).add_system(win_text);
     }
 }
 
@@ -55,6 +57,40 @@ fn setup_text(mut commands: Commands, asset_server: Res<AssetServer>) {
             position: UiRect {
                 top: Val::Percent(0.0),
                 right: Val::Percent(0.0),
+                ..default()
+            },
+            ..default()
+        }),
+    );
+}
+
+fn win_text(mut commands: Commands, asset_server: Res<AssetServer>, level_info: Res<LevelInfo>) {
+    if !level_info.is_changed() {
+        return;
+    }
+
+    if level_info.index != Some(level_info.total_levels as i32) {
+        return;
+    }
+    commands.spawn_bundle(
+        TextBundle::from_section(
+            "Congratulations! You win
+
+            Reload to play again",
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 35.0,
+                color: Color::WHITE,
+            },
+        )
+        .with_text_alignment(TextAlignment::CENTER_RIGHT)
+        .with_style(Style {
+            align_self: AlignSelf::FlexEnd,
+            position_type: PositionType::Absolute,
+
+            position: UiRect {
+                top: Val::Percent(40.0),
+                left: Val::Percent(40.0),
                 ..default()
             },
             ..default()
